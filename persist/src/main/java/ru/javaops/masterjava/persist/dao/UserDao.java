9c51d3ad97e8d2eac715e5e424e8dcef95728a3e
Artem Vogulev <art.vogulev@gmail.com>
2024-11-24 00:31:48 +0300

    add save users in batch mode

--- persist/src/main/java/ru/javaops/masterjava/persist/dao/UserDao.java
+++ persist/src/main/java/ru/javaops/masterjava/persist/dao/UserDao.java
@@ -2,6 +2,8 @@ package ru.javaops.masterjava.persist.dao;
 
 import com.bertoncelj.jdbi.entitymapper.EntityMapperFactory;
 import org.skife.jdbi.v2.sqlobject.*;
+import org.skife.jdbi.v2.sqlobject.customizers.BatchChunkSize;
+import org.skife.jdbi.v2.sqlobject.customizers.OutParameter;
 import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapperFactory;
 import ru.javaops.masterjava.persist.model.User;
 
@@ -15,7 +17,7 @@ public abstract class UserDao implements AbstractDao {
             int id = insertGeneratedId(user);
             user.setId(id);
         } else {
-            insertWitId(user);
+            insertWithId(user);
         }
         return user;
     }
@@ -25,10 +27,13 @@ public abstract class UserDao implements AbstractDao {
     abstract int insertGeneratedId(@BindBean User user);
 
     @SqlUpdate("INSERT INTO users (id, full_name, email, flag) VALUES (:id, :fullName, :email, CAST(:flag AS user_flag)) ")
-    abstract void insertWitId(@BindBean User user);
+    abstract void insertWithId(@BindBean User user);
 
     @SqlQuery("SELECT * FROM users ORDER BY full_name, email LIMIT :it")
     public abstract List<User> getWithLimit(@Bind int limit);
+
+    @SqlBatch("INSERT INTO users (full_name, email, flag) VALUES (:fullName, :email, CAST(:flag AS user_flag))")
+    public abstract void saveUsers(@BindBean List<User> users, @BatchChunkSize int chunk);
 
     //   http://stackoverflow.com/questions/13223820/postgresql-delete-all-content
     @SqlUpdate("TRUNCATE users")
--- web/upload/src/main/java/ru/javaops/masterjava/upload/UploadServlet.java
+++ web/upload/src/main/java/ru/javaops/masterjava/upload/UploadServlet.java
@@ -1,6 +1,8 @@
 package ru.javaops.masterjava.upload;
 
 import org.thymeleaf.context.WebContext;
+import ru.javaops.masterjava.persist.DBIProvider;
+import ru.javaops.masterjava.persist.dao.UserDao;
 import ru.javaops.masterjava.persist.model.User;
 
 import javax.servlet.ServletException;
@@ -21,7 +23,7 @@ import static ru.javaops.masterjava.common.web.ThymeleafListener.engine;
 public class UploadServlet extends HttpServlet {
 
     private final UserProcessor userProcessor = new UserProcessor();
-
+    private final UserDao dao = DBIProvider.getDao(UserDao.class);
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         final WebContext webContext = new WebContext(req, resp, req.getServletContext(), req.getLocale());
@@ -40,6 +42,7 @@ public class UploadServlet extends HttpServlet {
             }
             try (InputStream is = filePart.getInputStream()) {
                 List<User> users = userProcessor.process(is);
+                //dao.saveUsers(users, 1);
                 webContext.setVariable("users", users);
                 engine.process("result", webContext, resp.getWriter());
             }
