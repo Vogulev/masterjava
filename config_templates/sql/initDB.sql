af0108543d5d4f889cb9d3ee5d244d8d5c5e0507
Artem Vogulev <art.vogulev@gmail.com>
2024-12-21 19:56:31 +0700

    add cities, groups, projects to DB init script

--- config_templates/sql/initDB.sql
+++ config_templates/sql/initDB.sql
@@ -1,6 +1,8 @@
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS user_seq;
 DROP TYPE IF EXISTS user_flag;
+DROP TABLE IF EXISTS cities;
+DROP TABLE IF EXISTS groups;
 
 CREATE TYPE user_flag AS ENUM ('active', 'deleted', 'superuser');
 
@@ -10,7 +12,28 @@ CREATE TABLE users (
   id        INTEGER PRIMARY KEY DEFAULT nextval('user_seq'),
   full_name TEXT NOT NULL,
   email     TEXT NOT NULL,
-  flag      user_flag NOT NULL
+  flag      user_flag NOT NULL,
+  city      CHAR REFERENCES cities
 );
 
 CREATE UNIQUE INDEX email_idx ON users (email);
+
+CREATE TABLE cities (
+    id        CHAR PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TYPE group_type AS ENUM ('REGISTERING', 'CURRENT', 'FINISHED');
+
+CREATE TABLE groups (
+    id CHAR PRIMARY KEY,
+    user_id INTEGER references users,
+    project_name TEXT references projects,
+    name TEXT NOT NULL UNIQUE,
+    type group_type NOT NULL
+);
+
+CREATE TABLE projects (
+    name TEXT PRIMARY KEY,
+    description TEXT
+)
