fb473bb54e438ebdb0ddc53b376d66c12ce124b1
Artem Vogulev <art.vogulev@gmail.com>
2025-03-09 15:00:42 +0300

    mailProvider refactoring

--- services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/EmailProvider.java
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/EmailProvider.java
@@ -10,23 +10,19 @@ public class EmailProvider {
 
     public static Email initEmail() {
         Config mail = Configs.getConfig("mail.conf", "mail");
-        return initMail(mail.getString("host"), mail.getString("port"),
-                mail.getString("username"), mail.getString("password"),
-                mail.getString("fromName"), mail.getBoolean("debug"),
-                mail.getBoolean("useSSL"), mail.getBoolean("useTLS"));
+        return initMail(mail);
     }
 
-    private static Email initMail(String host, String port, String username, String password, String fromName,
-                                  boolean debug, boolean useSSL, boolean useTLS) {
+    private static Email initMail(Config mail) {
         Email email = new SimpleEmail();
-        email.setHostName(host);
-        email.setSslSmtpPort(port);
-        email.setAuthentication(username, password);
-        email.setSSLOnConnect(useSSL);
-        email.setStartTLSEnabled(useTLS);
-        email.setDebug(debug);
+        email.setHostName(mail.getString("host"));
+        email.setSslSmtpPort(mail.getString("port"));
+        email.setAuthentication(mail.getString("username"), mail.getString("password"));
+        email.setSSLOnConnect(mail.getBoolean("useSSL"));
+        email.setStartTLSEnabled(mail.getBoolean("useTLS"));
+        email.setDebug(mail.getBoolean("debug"));
         try {
-            email.setFrom(username, fromName);
+            email.setFrom(mail.getString("username"), mail.getString("fromName"));
         } catch (EmailException e) {
             throw new RuntimeException(e);
         }
